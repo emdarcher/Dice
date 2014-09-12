80abaca45b46d857b98122ea6c80feb0915853e1
Ethan Durrant <emd519@yahoo.com>
2014-09-12 10:51:34 -0700

    made basic stuff work for now

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 /*
 die face
 has 3 2-dot pairs that are always together,
@@ -31,19 +47,19 @@ byte[] die_bits = {
 
 Die thing1;
 
-void setup()
+public void setup()
 {
 	size(127,127);
 	thing1 = new Die(20,20);
 	noLoop();
 }
-void draw()
+public void draw()
 {
 	background(0);
 	thing1.roll();
 	thing1.show();
 }
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -65,14 +81,14 @@ class Die //models one single dice cube
 		Die_width = 60;
 		Die_dot_width = 15;
 	}
-	void roll()
+	public void roll()
 	{
 		//your code here
 
 		Die_val = (int)((Math.random()*6)+1);
 
 	}
-	void show()
+	public void show()
 	{
 		//your code here
 		fill(255);
@@ -101,4 +117,13 @@ class Die //models one single dice cube
 
 
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
